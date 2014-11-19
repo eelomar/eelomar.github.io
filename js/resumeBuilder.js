85fdcbaa7282f6c482e3c40ba644a13a85196c22
Eloy Martinez PC <emartinez@research.ait.ie>
2014-11-19 16:50:21 +0000

    hange made to resumeBuilder.js with my 4 objects after lesson 1 data types

---
+++ js/resumeBuilder.js
@@ -0,0 +1,135 @@
+//var email="eloy@email.com"
+//var newemail=email.replace("email","gmail");
+
+//console.log(email);
+//console.log(newemail);
+
+//var awesomeThoughts="I am Eloy and I am AWESOME!"
+//var funThoughts=awesomeThoughts.replace("AWESOME","FUN")
+
+
+//$("#main").append(funThoughts);
+
+//var formattedName=HTMLheaderName.replace("%data%", "Eloy Martinez")
+//var formattedRole=HTMLheaderRole.replace("%data%", "Master Engineer")
+
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+//var skills=["awesome","programming","Engineer"]
+
+//$("#main").append(skills[2]);
+
+//var bio = {
+//	"name":"Eloy",
+//	"age":25,
+//	"skills":skills[2],
+//	"welcome mesage": awesomeThoughts,
+//	"picture":"images/yegua.jpg"
+//};
+
+//$("#main").append(bio);
+
+//var work ={};
+//work.position="Master of universe";
+
+//var educa={};
+//educa["name"]="AIT";
+
+//$("#main").append(work["position"]);
+//$("#main").append(educa.name);
+var work={
+	"jobs":[
+	{
+		"employeer":"Ericsson",
+		"title":"Trainer Engineer",
+		"location":"Athlone",
+		"dates":"September 2013 - May 2015",
+		"description":"Develop some research in telecommunications for Ericsson "
+	},
+	{
+		"employeer":"Inescop",
+		"title":"Practics Engineer",
+		"location":"Elda",
+		"dates":"July 2011 - July 2011",
+		"description":"Work with a robotic arm and its sensors"
+	},
+	{
+		"employeer":"Escarche",
+		"title":"Helping boy",
+		"location":"Elda",
+		"dates":"Summers from 2007 to 2010",
+		"description":"Help to instal and repare A.A. and super friges"
+	}
+	]
+};
+var projects={
+	"projects":[
+	{
+		"title":"Map Reduce",
+		"dates":"September 2013 - January 2014",
+		"images":["images/hadoopCluster.jpg","images/structure.jpg","images/perfromance.jpg"],
+		"description":"Work with Map/reduce parallel programing mode and its issues with telecommunication´s data"
+	},
+	{
+		"title":"Outlier detection",
+		"dates":"February 2014 - February 2015",
+		"images":["images/DBSW.jpg","images/framework.jpg","images/bigdata.jpg"],
+		"description":"Develop an user interface to show the users abnormals terms from different data sources indexed in Solr"
+	}]
+};
+var bio={
+	"name":"Eloy Martinez Colomina",
+	"role":"Industrial Engineer",
+	"contact":{
+		"mobile":"003530851562822",
+		"email":"emartinez@research.ait.ie",
+		"github":"eelomar",
+		"location":"Athlone, Ireland"
+	},
+	"Skills":["Big Data","Data Mining","Java","Front-End Developer"],
+	"welcomeMessage":"Hello everyone, I hope you enjoy my page"
+};
+var educational={
+	"schools":[
+	{
+		"name":"Univerty polytechnic of Valencia ",
+		"location":"Valencia, Spain",
+		"degree":"Insdustrial Engineer",
+		"majors":["Electronics","Automatization","General industry"],
+		"dates":"September 2007 - June 2012",
+		"url":"http://www.upv.es"
+	},
+	{
+		"name":"Athlone Institute of Technology ",
+		"location":"Athlone, Ireland",
+		"degree":"Bachelor Software Engineer",
+		"majors":["Java","Server Side"],
+		"dates":"September 2012 - June 2013",
+		"url":"http://www.ait.ie"
+
+	},
+	{
+		"name":"IES La Torreta",
+		"location":"Elda, Spain",
+		"degree":"High school diploma",
+		"majors":["Technology"],
+		"dates":"September 2001 - June 2007",
+		"url":"http://ies-latorreta.es/portal/"
+
+	}
+	],
+	"onlineCourses":[{
+		"title":"Front-End Web Developer",
+		"school":"Udacity",
+		"dates":"November 2014 - OnLine",
+		"url":"https://www.udacity.com/"
+	},{
+		"title":"Advance Java",
+		"school":"Coursera",
+		"dates":"September 2014 - December 2014",
+		"url":"https://www.coursera.org/"
+	}]
+};
+
+//rellenalos con algo pollo
